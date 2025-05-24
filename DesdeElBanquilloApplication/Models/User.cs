83c41bb128311fa9df89d7625c5ede0e39415e7d
Camilyyyy <camilystefaniasoza@gmail.com>
2025-05-23 22:52:18 -0500

    Model User Update

--- DesdeElBanquilloApplication/Models/User.cs
+++ DesdeElBanquilloApplication/Models/User.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace DesdeElBanquilloApplication.Models
 {
@@ -9,17 +10,22 @@ namespace DesdeElBanquilloApplication.Models
 
         [Required]
         [StringLength(100)]
+        [DisplayName("Email User")]
         public string Email { get; set; }
 
         [Required]
         [StringLength(255)]
+        [DisplayName("Password User")]
         public string Password { get; set; }
 
         [StringLength(50)]
+        [DisplayName("Nombre User")]
         public string Nombre { get; set; }
 
+        [DisplayName("Register Date User")]
         public DateTime RegisterDate { get; set; } = DateTime.Now;
 
+        [DisplayName("Estado User")]
         public bool IsAdmin { get; set; } = false;
     }
 }
