e83940c84640274f97234c039275a95f5e254e93
Camilyyyy <camilystefaniasoza@gmail.com>
2025-05-23 22:26:25 -0500

    Add  UserModel to use ORM

---
+++ DesdeElBanquilloApplication/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DesdeElBanquilloApplication.Models
+{
+    public class User
+    {
+        [Key]
+        public int IdUser { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Password { get; set; }
+
+        [StringLength(50)]
+        public string Nombre { get; set; }
+
+        public DateTime RegisterDate { get; set; } = DateTime.Now;
+
+        public bool IsAdmin { get; set; } = false;
+    }
+}
