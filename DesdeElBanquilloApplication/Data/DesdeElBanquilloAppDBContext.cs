34ce75a7c1dd91b136568348b0682699e8bdfd8c
Camilyyyy <camilystefaniasoza@gmail.com>
2025-05-24 17:29:27 -0500

    Created Match Controller

---
+++ DesdeElBanquilloApplication/Data/DesdeElBanquilloAppDBContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DesdeElBanquilloApplication.Models;
+
+    public class DesdeElBanquilloAppDBContext : DbContext
+    {
+        public DesdeElBanquilloAppDBContext (DbContextOptions<DesdeElBanquilloAppDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DesdeElBanquilloApplication.Models.Match> Match { get; set; } = default!;
+    }
--- DesdeElBanquilloApplication/Program.cs
+++ DesdeElBanquilloApplication/Program.cs
@@ -1,4 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<DesdeElBanquilloAppDBContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DesdeElBanquilloAppDBContext") ?? throw new InvalidOperationException("Connection string 'DesdeElBanquilloAppDBContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
