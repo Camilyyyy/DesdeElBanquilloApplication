7ef0e27b045ad399b6848a9295f897f78f5c0208
jhonatanTipan <jhonatan.tipan@udla.edu.ec>
2025-07-13 17:36:28 -0500

    Migracion BD local pc JOEL

---
+++ DesdeElBanquilloApplication/Migrations/20250713223504_MigracionBD_LocaldeJoel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DesdeElBanquilloApplication.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigracionBD_LocaldeJoel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
